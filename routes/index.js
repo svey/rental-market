efdbbd853c50d4ad6c47c1aed51ff20829c8b56d
svey <marcus.svehlak@gmail.com>
2017-07-28 15:56:36 -0700

    Modified file structure & added /earnings post req

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,89 @@
-const routes = require('express').Router();
-const path = require('path')
+var router = require('express').Router();
+var path = require('path')
 
-const price = require('./price');
-const booking_rate = require('./booking_rate');
-const earnings = require('./earnings');
+var rentalAnalysis = require('../rentalAnalysis');
+var linearRegression = require('../linearRegression');
 
-routes.use('/price', price);
-routes.use('/booking_rate', booking_rate);
-routes.use('/earnings', earnings);
+var lRByZip = {};
+var bRByZip = {};
 
-module.exports = routes;
+// price endpoint
+router.route('/price').post(function(req, res) {
+    // request parameters
+    var zipcode = req.body.zipcode;
+    var bedroom_count = req.body.bedroom_count;
+
+    // check if the linear regression hash has info for the requested zip, if not add it
+    if(!lRByZip.hasOwnProperty(zipcode)) {
+      var { prices, bedrooms } = rentalAnalysis[zipcode]
+      lRByZip[zipcode] = linearRegression(prices, bedrooms);
+    }
+
+    // destructure linear regression object for requested zip and calculate price
+    var { slope, intercept } = lRByZip[zipcode];
+    var price = Math.round(slope * bedroom_count + intercept);
+
+    res.send({ price })
+  });
+
+// booking_rate endpoint
+router.route('/booking_rate').post(function(req, res) {
+    // request parameters
+    var zipcode = req.body.zipcode;
+    var bedroom_count = req.body.bedroom_count;
+
+    // check if our booking rate hash has info for the requested zip, if not add it
+    if(!bRByZip.hasOwnProperty(zipcode)) {
+      var { prices, reserved } = rentalAnalysis[zipcode]
+      bRByZip[zipcode] = Math.round(100 * (reserved.length/(prices.length + reserved.length))) / 100;
+    }
+
+    // calculate booking rate
+    var booking_rate = bRByZip[zipcode];
+
+    res.send({ booking_rate })
+  });
+
+router.route('/earnings').post(function(req, res) {
+    // request parameters
+    var parseDate = function(str) {
+      var ymd = str.split('-');
+      return new Date(ymd[0], ymd[1]-1, ymd[2]);
+    }
+
+    var numberOfDays = function(start, end) {
+      var millisecondsPerDay = 24 * 60 * 60 * 1000;
+      return Math.round((end - start) / millisecondsPerDay);
+    }
+
+    var start_date = parseDate(req.body.start_date); //'2015-01-01'
+    var end_date = parseDate(req.body.end_date); //'2015-07-01'
+    var zipcode = req.body.zipcode;
+    var bedroom_count = req.body.bedroom_count;
+
+    var days = numberOfDays(start_date, end_date)
+    
+    // check if the linear regression hash has info for the requested zip, if not add it
+    if(!lRByZip.hasOwnProperty(zipcode)) {
+      var { prices, bedrooms } = rentalAnalysis[zipcode]
+      lRByZip[zipcode] = linearRegression(prices, bedrooms);
+    }
+
+    // destructure linear regression object for requested zip and calculate price
+    var { slope, intercept } = lRByZip[zipcode];
+    var price = Math.round(slope * bedroom_count + intercept);
+
+
+    // check if our booking rate hash has info for the requested zip, if not add it
+    if(!bRByZip.hasOwnProperty(zipcode)) {
+      var { prices, reserved } = rentalAnalysis[zipcode]
+      bRByZip[zipcode] = Math.round(100 * (reserved.length/(prices.length + reserved.length))) / 100;
+    }
+
+    var booking_rate = bRByZip[zipcode];
+    var earnings = Math.round(booking_rate * days * price);
+
+    res.send({ earnings })
+  });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,58 +1,14 @@
 var express = require('express');
-var app = express();
+var routes = require('./routes');
 var bodyParser = require('body-parser');
 
-
-var rentalAnalysis = require('./rentalAnalysis');
-var linearRegression = require('./linearRegression');
-var lRByZipCode = {};
-var bRByZipCode = {};
+var app = express();
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use('/api', routes);
 
-var port = process.env.PORT || 3000;
-
-var router = express.Router();
-
-router.route('/price')
-  .post(function(req, res) {
-    // REQ PARAMS
-    var zipcode = req.body.zipcode;
-    var bedroom_count = req.body.bedroom_count;
-
-    // CHECK IF OUR LRHASH HAS LR OBJ FOR REQUEST'S ZIP
-    if(!lRByZipCode.hasOwnProperty(zipcode)) {
-      var { prices, bedrooms } = rentalAnalysis[zipcode]
-      lRByZipCode[zipcode] = linearRegression(prices, bedrooms);
-    }
-
-    // DESTRUCTURE LR DATA AND CALCULATE PRICE
-    var { slope, intercept } = lRByZipCode[zipcode];
-    var price = Math.round(slope * bedroom_count + intercept);
-
-    res.send({ price })
-  });
-
-  router.route('/booking_rate')
-    .post(function(req, res) {
-      // REQ PARAMS
-      var zipcode = req.body.zipcode;
-      var bedroom_count = req.body.bedroom_count;
-
-      // CHECK IF OUR BRHASH HAS BR FOR REQUEST'S ZIP
-      if(!bRByZipCode.hasOwnProperty(zipcode)) {
-        var { prices, reserved } = rentalAnalysis[zipcode]
-        bRByZipCode[zipcode] =  Math.round(100 * (reserved.length/(prices.length + reserved.length))) / 100;
-      }
-
-      // DESTRUCTURE LR DATA AND CALCULATE PRICE
-      var booking_rate = bRByZipCode[zipcode];
-
-      res.send({ booking_rate })
-    });
-
-app.use('/api', router);
-
-app.listen(port);
-console.log('it\'s alive... ' + port);
+app.listen(port, function() {
+  console.log('it\'s alive!');
+});
