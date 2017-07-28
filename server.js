dee05f80fad96e9767b09b9e69385aacef336a2e
svey <marcus.svehlak@gmail.com>
2017-07-28 05:25:14 -0700

    Updated to res w/ { price: price } for /price reqs

--- server.js
+++ server.js
@@ -10,39 +10,38 @@ var lRByZipCode = {};
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 
 var router = express.Router();
 
-router.use(function(req, res, next) {
-    console.log('incomming request.');
-    next();
-});
+// MIDDLEWARE TO SEE INCOMMING REQUEST IN THE TERMINAL
+
+// router.use(function(req, res, next) {
+//     console.log('incomming request.');
+//     next();
+// });
 
 router.route('/price')
   .post(function(req, res) {
-
+    // REQ PARAMS
     var zipcode = req.body.zipcode;
     var bedroom_count = req.body.bedroom_count;
 
+    // CHECK IF OUR LRHASH HAS LR OBJ FOR REQUEST'S ZIP
     if(!lRByZipCode.hasOwnProperty(zipcode)) {
-      var { prices, bedrooms } = rentalAnalysis[`${zipcode}`]
+      var { prices, bedrooms } = rentalAnalysis[zipcode]
       lRByZipCode[zipcode] = linearRegression(prices, bedrooms);
     }
 
+    // DESTRUCTURE LR DATA AND CALCULATE PRICE
     var { slope, intercept } = lRByZipCode[zipcode];
-    var estimate = Math.round(slope * bedroom_count + intercept);
+    var price = Math.round(slope * bedroom_count + intercept);
 
-    //var slopeIntercept = linearRegression(pricesArray, bedroomsArray);
-    res.send({ estimate })
+    res.send({ price })
 
-  })
-// more routes for our API will happen here
+  });
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
 app.use('/api', router);
 
-// START THE SERVER
 app.listen(port);
 console.log('it\'s alive... ' + port);
